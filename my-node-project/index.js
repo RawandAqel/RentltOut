a4762622d60089aba121b9fefa09bba1c75c64a7
aseelzaid <159944921+aseelzaid@users.noreply.github.com>
2024-11-04 01:05:43 +0200

    add new file

--- my-node-project/index.js
+++ my-node-project/index.js
@@ -79,8 +79,8 @@ app.use('/api', require('./routes/rentals_barters_managment/rentals_routes/get_u
 app.use('/api', require('./routes/rentals_barters_managment/rentals_routes/creation_rentals'));  // Rental-related routes
 app.use('/api', require('./routes/rentals_barters_managment/rentals_routes/status_update'));  //change (rentals-barters-item availability) status
 app.use('/api', require('./routes/rentals_barters_managment/delete_rentals'));  //delete rentals or barters
-
-
+app.use('/api', require('./routes/rentals_barters_managment/rentals_routes/status_update'));  //change (rentals-barters-item availability) status
+app.use('/api', require('./routes/rentals_barters_managment/update_rentals_informations'));  //change information in rentals-barters
 
 
 
